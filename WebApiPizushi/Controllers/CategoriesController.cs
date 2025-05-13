d66c585bd4412feaee26d9cf875ba43ee6574b34
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-05-13 21:04:57 +0300

    Is good categories get

---
+++ WebApiPizushi/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiPizushi.Data;
+using WebApiPizushi.Models.Category;
+
+namespace WebApiPizushi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController(AppDbContext context,
+    IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> List()
+    {
+        var model = await mapper.ProjectTo<CategoryItemModel>(context.Categories).ToListAsync();
+
+        return Ok(model);
+    }
+}
