9e04d981fee56971a74dcac14a464419f29a0b98
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-05-22 19:57:41 +0300

    add validation for category repeat

--- WebApiPizushi/Validators/Category/CategoryCreateValidator.cs
+++ WebApiPizushi/Validators/Category/CategoryCreateValidator.cs
@@ -1,23 +1,32 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using WebApiPizushi.Data;
 using WebApiPizushi.Models.Category;
 
 namespace WebApiPizushi.Validators.Category;
 
 public class CategoryCreateValidator : AbstractValidator<CategoryCreateModel>
 {
-    public CategoryCreateValidator()
+    public CategoryCreateValidator(AppDbContext db)
     {
         // валідація
         RuleFor(x => x.Name)
             .NotEmpty()
             .WithMessage("Name is required")
             .MaximumLength(250)
-            .WithMessage("Name has to be no longer than 250 charachters");
+            .WithMessage("Name has to be no longer than 250 charachters")
+            .MustAsync(async (name, cancellation) =>
+                    !await db.Categories.AnyAsync(c => c.Name == name, cancellation))
+                .WithMessage("Category with this name already exists"); ;
         RuleFor(x => x.Slug)
             .NotEmpty()
             .WithMessage("Slug is required")
             .MaximumLength(250)
-            .WithMessage("Slug has to be no longer than 250 charachters");
+            .WithMessage("Slug has to be no longer than 250 charachters")
+            .MustAsync(async (slug, cancellation) =>
+                    !await db.Categories.AnyAsync(c => c.Slug == slug, cancellation))
+                .WithMessage("Category with this slug already exists"); 
+
         RuleFor(x => x.ImageFile)
             .NotEmpty()
             .WithMessage("Image file is required");
