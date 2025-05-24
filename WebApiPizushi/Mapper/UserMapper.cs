0902b70e5b98e11b2096c4164b2269b221c5501e
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-05-24 20:01:40 +0300

    is good working seed data

---
+++ WebApiPizushi/Mapper/UserMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebApiPizushi.Data.Entities.Identity;
+using WebApiPizushi.Models.Seeder;
+
+namespace WebApiPizushi.Mapper;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<SeederUserModel, UserEntity>()
+            .ForMember(x=>x.UserName,opt=>opt.MapFrom(x=>x.Email));
+    }
+}
